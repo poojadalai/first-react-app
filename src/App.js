e63ccc38fdcec2f31f204bee946b37a39503b4f9
poojadalai <poojadalai@gmail.com>
2022-03-09 11:23:37 +0100

    added title component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import ArticleList from './components/ArticleList';
 import AwesomeAnimals from './components/AwesomeAnimals';
 import LikeButton from './components/LikeButton';
 import LikeCounter from './components/LikeCounter';
+import Title from './components/Title';
 
 function App() {
   return (
     <div className="App">
+
+      <Title text="MY FIRST REACT APP"/>
+      <div>============================</div>
       <ArticleList />
-      
+
       <div>============================</div>
       <AwesomeAnimals />
 
